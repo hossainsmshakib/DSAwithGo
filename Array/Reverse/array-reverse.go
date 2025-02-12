f9f14a22819e6bff822d8382844cd8b65ec5b23e
hossainsmshakib <smsakib2403@gmail.com>
2025-02-12 07:52:10 +0600

    updated array-reverse

--- Array/Reverse/array-reverse.go
+++ Array/Reverse/array-reverse.go
@@ -1,3 +1,6 @@
+// Time - O(N)
+// Space - O(N)
+
 package main
 
 import "fmt"
@@ -16,7 +19,7 @@ func reverse(arr []int, left int, right int) {
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7}
 	fmt.Println("Original Array: ", arr)
-	
+
 	// passing the array and left, right position
 	reverse(arr, 0, len(arr) - 1)
 	fmt.Println("Reversed Array: ", arr)
