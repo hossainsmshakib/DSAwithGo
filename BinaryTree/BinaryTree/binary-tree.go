db10d95cb048b863335664df04e5b053dfbbce4f
hossainsmshakib <smsakib1403@gmail.com>
2025-02-18 11:38:12 +0600

    func inserNode added

--- BinaryTree/BinaryTree/binary-tree.go
+++ BinaryTree/BinaryTree/binary-tree.go
@@ -2,10 +2,31 @@ package main
 
 type TreeNode struct {
 	value int
-	Left  *TreeNode
-	Right *TreeNode
+	left  *TreeNode
+	right *TreeNode
+}
+
+func insertNode(root *TreeNode, newValue int) *TreeNode {
+	if root == nil {
+		return &TreeNode{value: newValue}
+	}
+
+	if newValue < root.value {
+		root.left = insertNode(root.left, newValue)
+	} else {
+		root.right = insertNode(root.right, newValue)
+	}
+	return root
 }
 
 func main() {
+	var treeRoot *TreeNode
 
+	treeRoot = insertNode(treeRoot, 5)
+	treeRoot = insertNode(treeRoot, 3)
+	treeRoot = insertNode(treeRoot, 7)
+	treeRoot = insertNode(treeRoot, 2)
+	treeRoot = insertNode(treeRoot, 4)
+	treeRoot = insertNode(treeRoot, 6)
+	treeRoot = insertNode(treeRoot, 8)
 }
